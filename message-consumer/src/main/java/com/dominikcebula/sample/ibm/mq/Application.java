e276d54503acada901fda03b26a3f1ed183499b9
Dominik Cebula <dominikcebula@gmail.com>
2025-09-05 01:14:02 +0200

    java-spring-boot-ibm-mq

--- message-consumer/src/main/java/com/dominikcebula/sample/ibm/mq/Application.java
+++ message-consumer/src/main/java/com/dominikcebula/sample/ibm/mq/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
 
-import java.io.File;
-
 @SpringBootApplication
 @EnableJms
 public class Application {
